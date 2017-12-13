aa921e841ff76a083a01ffab8b66f21fb6394b2d
Abdoul Dolo <abdouldolo1@gmail.com>
2017-12-12 21:13:19 -0500

    a try

---
+++ HelloWorld/src/Program/HelloWorld.java
@@ -0,0 +1,11 @@
+package Program;
+
+public class HelloWorld {
+	public static void myMethod() {
+		System.out.println("This is my method!");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
