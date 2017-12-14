85f74ca2d8561ebe64075e771b576f5eeabbbc11
Abdoul Dolo <abdouldolo1@gmail.com>
2017-12-14 07:27:29 -0500

    Basic java connection to jdbc

---
+++ jdbcintro/src/main/java/com/ex/test/MainTest.java
@@ -0,0 +1,32 @@
+package com.ex.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ex.dao.DAO;
+import com.ex.dao.DAOImpl;
+import com.ex.pojos.Artist;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		DAO dao = new DAOImpl();
+	
+		/*	ArrayList<Artist> artists = dao.getArtists();
+	
+		for(Artist art : artists){
+		System.out.println(art);		}
+		Artist art = dao.getArtistById(4);
+		System.out.println(art);
+		
+		System.out.println(dao.addArtist("Abdoul Dolo"));
+		dao.updateArtist(1, "TESTING");
+		System.out.println("done"); */
+	
+	List<Artist> artists = dao.getArtists();
+	for(Artist a : artists){
+		System.out.println(a);
+	}
+	}
+
+}
